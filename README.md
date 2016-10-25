e283cb4efa7a586246c97ca8fbdcddb3ef270aa5
Hassan Somashekara <somash@amazon.com>
2016-10-25 00:55:00 -0700

    Setup React Webpack Development Environment.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-starter-template
 Repository to provide the starter template for starting a React Project
+
+Run 'npm install' to install all the required packages and then run 'npm start' to run the development server using webpack.
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// This is the file that compiles all of the javascript, jsx down to javascript and it launches the development server.
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        path: './',
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 3000
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
